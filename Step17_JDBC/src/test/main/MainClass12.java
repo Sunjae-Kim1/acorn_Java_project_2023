7e85c197c2eb53079b252ca0bfc290275f396016
kimsunjae <ksj100301@gmail.com>
2023-05-16 09:06:03 +0900

    Step12 MainClass12 update

--- Step17_JDBC/src/test/main/MainClass12.java
+++ Step17_JDBC/src/test/main/MainClass12.java
@@ -6,16 +6,19 @@ import test.dto.MemberDto;
 public class MainClass12 {
 	public static void main(String[] args) {
 		// 추가할 회원 정보
+		int num = 1;
 		String name = "덩어리";
 		String addr = "목동";
 		
 		// 추가할 회원 정보를 MemberDto 객체에 대입
 		MemberDto dto = new MemberDto();
+		dto.setNum(num);
 		dto.setName(name);
 		dto.setAddr(addr);
 		
 		// DB 에 저장
 		new MemberDao().insert(dto);
 		new MemberDao().update(dto);
+		new MemberDao().delete(dto);
 	}
 }
